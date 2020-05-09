259152e6f6a6dd1819bcae9cc82ca4232a4e3b25
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-05-09 01:36:26 +0100

    Add hero section

--- resources/views/partials/main/hero.blade.php
+++ resources/views/partials/main/hero.blade.php
@@ -1,5 +1,45 @@
-<h1 class="title"> Club Scientifique de l'ESI </h1>
-<p>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-    magna aliquyam erat, sed diam voluptua. At vero eos et
-</p>
+<div class="hero-image">
+    <br>
+<br>
+<br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-6 col-sm-12">
+                <h1 class="title"> Club Scientifique de l'ESI </h1>
+                <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et
+                </p>
+                <a href="http://cse.club" target="_blank">
+                    <button>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                            JOIN US
+                    </button>
+                </a>
+                <p id="date">
+                    <span id="spanDate"></span>
+                </p>
+            </div>
+            <div class="col-lg-4 col-sm-12">
+            </div>
+            <div class="col-lg-1"></div>
+        </div>
+        
+    </div>
+</div>
+
+<script type="text/javascript">
+    var x = setInterval(function() {
+        var months = ['January','February','March','April','May','June','July',
+        'August','September','October','November','December'];
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];    
+        var today = new Date();
+        document.getElementById("spanDate").innerHTML = days[today.getDay()] + ", " + months[today.getMonth()] + " " + today.getDate()+ ", " + today.getFullYear()
+        + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    }, 1000)
+        
+</script>
