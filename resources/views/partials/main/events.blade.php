b06e0270a10b500235d2177906a0ac971f7d5a64
Sehdi Nassim <hm_sehdi@esi.dz>
2020-05-10 01:51:23 +0100

    Started the events section

---
+++ resources/views/partials/main/events.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col- title">
+            Events.
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <p class='col-9 events-p'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint non corporis d
+            ignissimos excepturi eaque omnis molestiae facilis laudantium accusamus ad? sdvjksdfjkb haya
+            tar sdvjksd jsdjvkn
+        </p>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-auto">
+            <button>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>	
+                BUTTON
+            </button>
+        </div>
+    </div>
+</div>
+
