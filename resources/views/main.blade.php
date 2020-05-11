eb0653fcb2aec234577256f9acba0df96aeb6519
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2020-05-11 16:57:47 +0100

    Add Footer

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -31,7 +31,10 @@
 {{-- Include Scripts that are used only in this page in this section --}}
 @section('scripts')
 <script src="{{asset('js/faq.js')}}"></script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbkAc5WgnIYDkqCPo0c5beRSFjtDcOsOM &callback=initMap"></script>
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbkAc5WgnIYDkqCPo0c5beRSFjtDcOsOM &callback=initMap">
+</script>
 <script src="{{asset('js/init.js')}}"></script>
+<script src="{{asset('js/up.js')}}"></script>
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,91 @@
-footer
+<footer>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-sm-12">
+                <div class="row mt-5 mb-3">
+                    <div class="col-12">
+                        <p class="title">
+                            Follow Us.
+                        </p>
+                    </div>
+                </div>
+                <div class="row mb-4 justify-content-md-center">
+                    <div class="col-lg-10">
+                        <div class="social-btns col-sm">
+                            <div class="row">
+                                <div class="col"><a class="btn" href="https://www.facebook.com/club.scientifique.esi/"
+                                        target="_blank"><i class="fab fa-facebook-f"></i></a></div>
+                                <div class="col"><a class="btn"
+                                        href="https://www.youtube.com/channel/UCHgeF6ELJW0Pt1vYoAomCig"
+                                        target="_blank"><i class="fab fa-youtube"></i></a></div>
+                                <div class="col"><a class="btn" href="https://twitter.com/CSESI_Club" target="_blank"><i
+                                            class="fab fa-twitter"></i></a></div>
+                                <div class="col"><a class="btn" href="https://www.instagram.com/cse.club/"
+                                        target="_blank"><i class="fab fa-instagram"></i></a></div>
+                                <div class="col"><a class="btn" href="https://www.linkedin.com/company/cse-club/"
+                                        target="_blank"><i class="fab fa-linkedin-in"></i></a></div>
+                                <div class="col"><a class="btn" href="https://medium.com/@cse_18061" target="_blank"><i
+                                            class="fab fa-medium-m"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-12">
+                        <p class="footer_made">Made with ❤️ by <img src="{{asset('img/cse-footer.png')}}"></p>
+                    </div>
+                </div>
+                <div class="row mb-4 links text-left">
+                    <div class="col-lg-4 mt-3">
+                        <h3 class="bold">About CSE</h3>
+                        <a href="http://" target="_blank">
+                            <p>About</p>
+                        </a>
+                        <a href="http://" target="_blank">
+                            <p>Vision</p>
+                        </a>
+                        <a href="http://" target="_blank">
+                            <p>Projects</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 mt-3">
+                        <h3 class="bold">CSE Events</h3>
+                        <a href="http://" target="_blank">
+                            <p>HackIT</p>
+                        </a>
+                        <a href="http://" target="_blank">
+                            <p>CasbahTech hackathon</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 mt-3">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h3 class="bold">Resources</h3>
+                                <a href="http://" target="_blank">
+                                    <p>FAQ</p>
+                                </a>
+                                <a href="http://" target="_blank">
+                                    <p>Code of Conduct</p>
+                                </a>
+                            </div>
+                            <div class="col-lg-12">
+                                <h3 class="bold">Partner</h3>
+                                <a href="http://" target="_blank">
+                                    <p>Become a partner</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="copyright">Copyright © 2019 Club Scientifique de l'ESI. All
+                            Rights Reserved. | Privacy Policy</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<a id="up"></a>
