90e2716cdfae97a3b55ede7c44b7ef039b06a089
Alaeddine <ia_nasri@esi.dz>
2020-05-13 15:14:06 +0200

    Photos without fade

--- resources/views/partials/main/about-us.blade.php
+++ resources/views/partials/main/about-us.blade.php
@@ -1,9 +1,10 @@
-<div class="row">
+<div class="row" id="aboutus">
+    <div class="col-lg-1" > </div>
     <div class="col-lg-6" >
-        <h1 class="title">About Us.</h1>
-        <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit.
-        </p>
+
+        <img src="{{asset('img/team1.jpg')}}" alt="team1" class="imgfade" id="img1" >
+        <img src="{{asset('img/team2.jpg')}}" alt="team2" class="imgfade" id="img2">    
+     
     </div>
     <div class="col-lg-4" >
         <h1 class="title">About Us.</h1>
@@ -11,4 +12,13 @@
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit.
         </p>
     </div>
+    <div class="col-lg-1" > </div>
 </div>
+
+    <div class="row" id="idk">
+    <div class="col-lg-1" > </div>
+    <div class="col-lg-6" >
+        
+       <img src="{{asset('img/group.png')}}" alt="idk" class="idk">
+    </div>
+    </div>
