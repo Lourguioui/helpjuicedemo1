c3f6b135c0aa7443f10f18a746d73987ef5996a9
Alaeddine <ia_nasri@esi.dz>
2020-05-13 21:57:04 +0200

    Ball animation

--- resources/views/partials/main/about-us.blade.php
+++ resources/views/partials/main/about-us.blade.php
@@ -1,25 +1,26 @@
 <div class="row" id="aboutus">
-    <div class="col-lg-1" > </div>
-    <div class="col-lg-6" >
+    <div class="col-lg-1" ></div>
+    <div class="col-lg-5" id="images" >
         <div class="img1"></div>
         <div class="img2"></div>
         <!-- <img src="{{asset('img/team1.jpg')}}" alt="team1" class="imgfade" id="img1" >
         <img src="{{asset('img/team2.jpg')}}" alt="team2" class="imgfade" id="img2">     -->
      
     </div>
+    <div class="col-lg-1" ></div>
     <div class="col-lg-4" >
         <h1 class="title">About Us.</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit.
         </p>
     </div>
-    <div class="col-lg-1" > </div>
+    <div class="col-lg-1" ></div>
 </div>
 
     <div class="row" id="non-specified">
     <div class="col-lg-1" > </div>
     <div class="col-lg-6" >
         <div class="circle"><p>;</p></div>
-       <img src="{{asset('img/group.png')}}" alt="idk" class="non-specified">
+       <img src="{{asset('img/Shelf.png')}}" alt="idk" class="non-specified">
     </div>
     </div>
