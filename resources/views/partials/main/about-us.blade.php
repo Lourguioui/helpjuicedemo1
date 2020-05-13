3960c09695015b07aae6b1a797ea184f6e8cce92
Alaeddine <ia_nasri@esi.dz>
2020-05-14 01:18:10 +0200

    Fixing images size problem + ball animation fixed + album animation

--- resources/views/partials/main/about-us.blade.php
+++ resources/views/partials/main/about-us.blade.php
@@ -1,6 +1,6 @@
 <div class="row" id="aboutus">
     <div class="col-lg-1" ></div>
-    <div class="col-lg-5" id="images" >
+    <div class="col-lg-4" id="images" >
      <div class="img1"></div>
         <div class="img2"></div> 
         <!-- <img src="{{asset('img/team1.jpg')}}" alt="team1" class="imgfade" id="img1" >
@@ -8,7 +8,7 @@
      
     </div>
     <div class="col-lg-1" ></div>
-    <div class="col-lg-4" >
+    <div class="col-lg-5" >
         <h1 class="title">About Us.</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit.
@@ -16,11 +16,16 @@
     </div>
     <div class="col-lg-1" ></div>
 </div>
-
-    <div class="row" id="non-specified">
+<div class="row" id="non-specified">
     <div class="col-lg-1" > </div>
+    <div class="col-lg-5" >
+        <div class="circle"></div>
+        <div class="shadow"></div>
+    </div>
+</div>
+<div class="row" id="wall" >
+    
     <div class="col-lg-6" >
-        <div class="circle"><p>;</p></div>
        <img src="{{asset('img/Shelf.png')}}" alt="idk" class="non-specified">
     </div>
-    </div>
+</div>
