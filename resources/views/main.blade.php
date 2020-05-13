dec7fbdad8ef036c94684db7caeeabe62bb44357
Alaeddine <ia_nasri@esi.dz>
2020-05-13 16:09:50 +0200

    Aboutus.css file

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,7 @@
 
 {{-- Include Styles that are used only in this page in this section --}}
 @section('styles')
-
+<link rel="stylesheet" href="{{asset('css/AboutUs.css')}}">
 @endsection
 
 {{-- Each section of this page should be in a file, the file must be in this directory partials/{page-name}/ --}}
