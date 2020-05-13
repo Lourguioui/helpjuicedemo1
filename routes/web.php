79533858111a6e5711f0e52659bc479dd50f6c30
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2020-05-13 14:42:31 +0100

    Add routes names

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('main');
-});
+})->name('main');
 
 Route::get('/our-vision', function () {
     return view('our-vision');
-});
+})->name('our-vision');
 
 Route::get('/register', function () {
     return view('register');
-});
+})->name('register');
 
 Route::get('/events', function () {
     return view('events');
-});
+})->name('events');
 
 Route::get('/projects', function () {
     return view('projects');
-});
+})->name('projects');
 
 Route::get('/media-coverage', function () {
     return view('media-coverage');
-});
+})->name('media');
 
 Route::get('/become-partner', function () {
     return view('become-partner');
@@ -43,4 +43,4 @@ Route::get('/become-partner', function () {
 
 Route::get('/members', function () {
     return view('members');
-});
+})->name('members');
