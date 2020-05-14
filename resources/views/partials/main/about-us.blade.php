fcd8885b6a8933615fa3a57230b82d13443d9bed
Alaeddine <ia_nasri@esi.dz>
2020-05-14 20:07:34 +0200

    space between text and images

--- resources/views/partials/main/about-us.blade.php
+++ resources/views/partials/main/about-us.blade.php
@@ -9,8 +9,8 @@
         <img src="{{asset('img/team2.jpg')}}" alt="team2" class="imgfade" id="img2">     -->
      
     </div>
-    <div class="col-lg-1" ></div>
-    <div class="col-lg-5" >
+    <div class="col-lg-2"></div>
+    <div class="col-lg-5" id="aboutustext" >
         <h1 class="title">About Us.</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit.
@@ -22,7 +22,7 @@
     <div class="col-lg-1" > </div>
     <div class="col-lg-5" >
         <div class="circle"></div>
-        <div class="shadow"></div>
+
     </div>
 </div>
 <div class="row" id="wall" >
