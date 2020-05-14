119d9259a1530542d15baf5a49aff324e3361886
Sehdi Nassim <hm_sehdi@esi.dz>
2020-05-14 01:02:48 +0100

    Added horizontal scroll and events data

--- public/js/events.js
+++ public/js/events.js
@@ -1,8 +1,103 @@
 let root = document.documentElement;
 
 var currentIndex = 0
+var currentRow = 1
+var scroll_to_left = false
+var is_selected = "is-Selected"
 
 var indexesInRight = []
+
+// This array is our events' data
+var events = [
+    {
+        name: 'Hack !T',
+        mini_logo_className: 'hackit-mini-logo',
+        mini_logo: 'img/logo.svg',
+        logo: './img/Groupe 37629.png',
+        alt: 'Hack !T logo',
+        logo_className: 'hackit-logo',
+        video: './img/Image 5.png'
+    },
+    {
+        name: 'Leapfrog',
+        mini_logo_className: 'leapfrog-mini-logo',
+        mini_logo: './img/Groupe 10158.svg',
+        logo: './img/Groupe 37630.png',
+        logo_className: 'leapfrog-logo',
+        alt: 'Leapfrog logo',
+        video: './img/Image 1.png'
+    },
+    {
+        name: 'ConfsDay',
+        mini_logo_className: 'confsday-mini-logo',
+        mini_logo: './img/Groupe 10161.svg',
+        logo: './img/Groupe 10161.svg',
+        logo_className: 'confsday-logo',
+        alt: 'ConfsDay logo',
+        video: './img/Image 1.png'
+    },
+    {
+        name: 'Local Hack Day',
+        mini_logo_className: 'lhd-mini-logo',
+        mini_logo: 'img/Groupe 37547.svg',
+        logo: './img/Asset 2.png',
+        logo_className: 'lhd-logo',
+        alt: 'LHD logo',
+        video: './img/Image 1.png'
+    },
+    {
+        name: 'Casbahtech <br/> Hackathon',
+        mini_logo_className: 'casbahtech-mini-logo',
+        mini_logo: './img/Groupe 10164.svg',
+        logo: './img/Groupe 37631.png',
+        logo_className: 'casbahtech-logo',
+        alt: 'Casbahtch logo',
+        video: './img/Image 1.png'
+    },
+    {
+        name: 'Smart Mobility <br/> Challenge',
+        mini_logo_className: 'smc-mini-logo',
+        mini_logo: 'img/Rectangle 55.png',
+        logo: 'img/Rectangle 55.png',
+        logo_className: 'smc-logo',
+        alt: 'SMC logo',
+        video: './img/Image 1.png'
+    },
+]
+events.map((element, i) => {
+    if (i === 1) {
+        is_selected = ''
+    }
+    if (i === 0) {
+        document.querySelector('.big-logo-circle').innerHTML += '<img class="big-logo in-middle ' + element.logo_className + '" src="' + element.logo + '" alt="' + element.alt + '"/>'
+        document.querySelector('.pictures-container').firstElementChild.innerHTML += '<img src="' + element.video + '" alt="event-picture" class="event-picture is-showed"/>'
+    } else {
+        document.querySelector('.big-logo-circle').innerHTML += '<img class="big-logo in-left ' + element.logo_className + '" src="' + element.logo + '" alt="' + element.alt + '"/>'
+        document.querySelector('.pictures-container').firstElementChild.innerHTML += '<img src="' + element.video + '" alt="event-picture" class="event-picture "/>'
+    }
+    document.querySelector('.left-side-container').innerHTML += "<div class='event-item " + is_selected + "'>" +
+        "<span class='event-item-top'>" +
+        "<span></span>" +
+        "<span></span>" +
+        '<img src="' + element.mini_logo + '" alt="' + element.alt + '" class="' + element.mini_logo_className + ' mini-logo" />' +
+        '</span>' +
+        "<p class='event-item-title'>" + element.name + "</p>" +
+        "<span class='event-item-bottom'>" +
+        "<span></span>" +
+        "<span></span>" +
+        "<span></span>" +
+        "</span>" +
+        "</div>"
+})
+
+window.addEventListener('resize', () => {
+    document.querySelector('.left-side-container').scrollTo({
+        left: scroll_to_left ? (currentRow - 1) * document.querySelector('.left-side-container').clientWidth * 5 / 6 : (currentRow - 3) * document.querySelector('.left-side-container').clientWidth * 5 / 6,
+        behavior: 'smooth'
+
+    })
+})
+
 
 function getDistance(e1, e2) {
     var x1 = e1.getBoundingClientRect().left
@@ -10,20 +105,20 @@ function getDistance(e1, e2) {
     return Math.abs(x1 - x2)
 }
 
-function setPointerEvents(className, state){
+function setPointerEvents(className, state) {
     document.querySelectorAll(className).forEach(element => {
-            element.setAttribute('pointer-events', state)
-        })
+        element.setAttribute('pointer-events', state)
+    })
 }
 
 function animateLogos(j) {
     if (j !== currentIndex) {
-        setPointerEvents('.mini-logo','none')
+        setPointerEvents('.mini-logo', 'none')
         if (!indexesInRight.includes(j)) {
             document.querySelectorAll('.big-logo')[currentIndex].classList.replace('in-middle', 'in-right')
             indexesInRight.unshift(currentIndex)
             document.querySelectorAll('.big-logo')[j].classList.replace('in-left', 'in-middle')
-            setPointerEvents('.mini-logo','all')
+            setPointerEvents('.mini-logo', 'all')
         } else {
             document.querySelectorAll('.big-logo')[currentIndex].classList.replace('in-middle', 'in-left')
             for (let i = 0; i < indexesInRight.indexOf(j) + 1; i++) {
@@ -33,28 +128,40 @@ function animateLogos(j) {
                         document.querySelectorAll('.big-logo')[element].classList.replace('in-right', 'in-middle')
                         indexesInRight.shift()
                         setTimeout(() => {
-                            setPointerEvents('.mini-logo','all')
+                            setPointerEvents('.mini-logo', 'all')
                         }, 300)
                     } else {
-                        document.querySelectorAll('.big-logo')[element].setAttribute('transition-duration','.6s')
+                        document.querySelectorAll('.big-logo')[element].setAttribute('transition-duration', '.6s')
                         document.querySelectorAll('.big-logo')[element].classList.replace('in-right', 'in-left')
-                        setTimeout(()=>{
-                            document.querySelectorAll('.big-logo')[element].setAttribute('transition-duration','.3s')
-                        },600)
+                        setTimeout(() => {
+                            document.querySelectorAll('.big-logo')[element].setAttribute('transition-duration', '.3s')
+                        }, 600)
                         indexesInRight.shift()
                     }
-                }, 150*(i+1))
+                }, 150 * (i + 1))
             }
         }
     }
 }
-
 document.querySelectorAll('.mini-logo').forEach(
     (element, i) => {
         element.addEventListener('click', (e) => {
             animateLogos(i)
             preSelected = document.querySelectorAll('.is-Selected')[0]
             newSelected = element.parentNode.parentNode
+            if (i % 5 === 0 && i !== 0 && i !== document.querySelectorAll('.mini-logo').length - 1 ||
+                (i === document.querySelectorAll('.mini-logo').length - 6 && currentRow === parseInt((document.querySelectorAll('.mini-logo').length - 1) / 5 + 1))) {
+                scroll_to_left = i / 5 === currentRow ? true : false
+                setTimeout(() => {
+                    newSelected.parentNode.scrollTo({
+                        left: i / 5 === currentRow ? (currentRow) * document.querySelector('.left-side-container').clientWidth * 5 / 6 : (currentRow - 2) * document.querySelector('.left-side-container').clientWidth * 5 / 6,
+                        behavior: 'smooth'
+
+                    })
+                    currentRow = i / 5 === currentRow ? currentRow + 1 : currentRow - 1
+                }, 300)
+            }
+
             if (i - currentIndex > 2) {
                 currentIndex = i
                 root.style.setProperty('--offset', '-' + getDistance(newSelected, preSelected) + "px");
