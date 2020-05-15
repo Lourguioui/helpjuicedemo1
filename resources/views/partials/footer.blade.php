beb99bcfa2a18bf11031c6a45318b05b9a7a6fcc
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-05-15 22:03:15 +0100

    Edit footer + navbar

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,6 +7,16 @@
                         <h3 class="title">
                             Follow Us.
                         </h3>
+                        <div class="input-group mb-3" id="email">
+                            <div class="input-group-prepend">
+                              <span class="input-group-text" id="inputGroup-sizing-default"><img src="{{asset('img/communications.svg')}}"></span>
+                            </div>
+                            <input type="text" class="form-control" aria-label="Default" style="border-style: hidden;"
+                            aria-describedby="inputGroup-sizing-default" placeholder="Enter you Email to subscribe to the newsletter">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="point-to"><img src="{{asset('img/point-to.svg')}}"></span>
+                            </div>
+                        </div>                          
                     </div>
                 </div>
                 <div class="row mb-4 justify-content-md-center">
