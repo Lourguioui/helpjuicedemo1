74d10dd836d40d11c52d36ac389dab924f54abcc
Yasser El Habib DRIF <gy_drif@esi.dz>
2020-07-15 14:46:59 +0100

    Added FAQ questions

--- resources/views/partials/main/faq.blade.php
+++ resources/views/partials/main/faq.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-12">
                     <h3 class="title text-center">
-                        FAQ.
+                    Frequently Asked Questions (FAQ)
                     </h3>
                 </div>
             </div>
@@ -12,7 +12,7 @@
                 <div class="accordion">
                     <div class="accordion-item">
                         <div class="accordion-item-header">
-                            What's a Hackathon ?
+                        How can you join us ?
                             <div class="neuo-checkbox">
                                 <label>
                                     <input type="checkbox" name="">
@@ -24,15 +24,15 @@
                         </div>
                         <div class="accordion-item-body">
                             <p class="accordion-item-body-content">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias culpa illo
-                                repellendus laboriosam? Architecto enim dolorem vitae, nesciunt vel quam fugiat
-                                impedit sunt porro sit. Aperiam perferendis quas modi earum!
+                            It's quite simple!
+At the start of each academic year, the CSE organizes a welcome-day with the aim of meeting those who want to be part of the CSE adventure and persuading those who still have doubts by presenting the club as well as its history, its departments, members ...
                             </p>
                         </div>
                     </div>
+
                     <div class="accordion-item">
                         <div class="accordion-item-header">
-                            What's a Hackathon ?
+                        Who can join us?
                             <div class="neuo-checkbox">
                                 <label>
                                     <input type="checkbox" name="">
@@ -44,12 +44,125 @@
                         </div>
                         <div class="accordion-item-body">
                             <p class="accordion-item-body-content">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias culpa illo
-                                repellendus laboriosam? Architecto enim dolorem vitae, nesciunt vel quam fugiat
-                                impedit sunt porro sit. Aperiam perferendis quas modi earum!
+                            Everyone can do it ! you do not have to be a student at ESI, it is enough that you are well motivated and interested in our field.
                             </p>
                         </div>
                     </div>
+
+
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                        What is our area of ​​work?
+                            <div class="neuo-checkbox">
+                                <label>
+                                    <input type="checkbox" name="">
+                                    <div class="icon-box">
+                                        <i class="fas fa-plus" aria-hidden="true"></i>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="accordion-item-body">
+                            <p class="accordion-item-body-content">
+                            The CSE is mainly oriented towards IT and all what's related to new technologies, we organize events and build Tech projects in any field.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                        why join us?
+                            <div class="neuo-checkbox">
+                                <label>
+                                    <input type="checkbox" name="">
+                                    <div class="icon-box">
+                                        <i class="fas fa-plus" aria-hidden="true"></i>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="accordion-item-body">
+                            <p class="accordion-item-body-content">
+                            At CSE you will learn and develop leadership skills such as decision making, problem solving, delegation and organization.
+you will of course acquire skills and experiences which will complement your university course, meet new people and live new experiences
+and, of course, have fun!                            </p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                        Do I have to be a CSE member to attend the events?
+                            <div class="neuo-checkbox">
+                                <label>
+                                    <input type="checkbox" name="">
+                                    <div class="icon-box">
+                                        <i class="fas fa-plus" aria-hidden="true"></i>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="accordion-item-body">
+                            <p class="accordion-item-body-content">
+                            Not necessarily ! you can be external to the CSE and of course access our events and workshops, you just need to register via the links that we share.                            </p>
+                        </div>
+                    </div>
+
+
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                        What is a hackathon?
+                            <div class="neuo-checkbox">
+                                <label>
+                                    <input type="checkbox" name="">
+                                    <div class="icon-box">
+                                        <i class="fas fa-plus" aria-hidden="true"></i>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="accordion-item-body">
+                            <p class="accordion-item-body-content">
+                            The word is a concatenation of two terms, "hack" and "marathon", it refers to an event during which teams (composed of developers, but sometimes also designers and project managers) must develop an IT project, in general software or application. They must do so over a limited, and generally short, period (a day, a night, a weekend).
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                        Is it free ?
+                            <div class="neuo-checkbox">
+                                <label>
+                                    <input type="checkbox" name="">
+                                    <div class="icon-box">
+                                        <i class="fas fa-plus" aria-hidden="true"></i>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="accordion-item-body">
+                            <p class="accordion-item-body-content">
+                            Of course ! all of our events are totally free, we are doing this for one ultimate goal which is developing the student's tech community and building the best solutions and projects for our country.
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <div class="accordion-item-header">
+                        Question not found?
+                            <div class="neuo-checkbox">
+                                <label>
+                                    <input type="checkbox" name="">
+                                    <div class="icon-box">
+                                        <i class="fas fa-plus" aria-hidden="true"></i>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="accordion-item-body">
+                            <p class="accordion-item-body-content">
+                            If your question is not listed here, please feel free to reach out to us at cse@esi.dz or on our social media.
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
