6692b7eda2f2e44c62e34399ed6e811de0fb1296
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2020-07-15 02:52:29 +0100

    add hero main animation and change main color

--- resources/views/partials/main/hero.blade.php
+++ resources/views/partials/main/hero.blade.php
@@ -4,8 +4,9 @@
             <div class="col-lg-6 col-sm-12">
                 <h1 class="title"> Club Scientifique de l'ESI </h1>
                 <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                magna aliquyam erat, sed diam voluptua. At vero eos et
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                    labore et dolore
+                    magna aliquyam erat, sed diam voluptua. At vero eos et
                 </p>
                 <a href="http://cse.club" target="_blank">
                     <button>
@@ -13,7 +14,7 @@
                         <span></span>
                         <span></span>
                         <span></span>
-                            JOIN US
+                        JOIN US
                     </button>
                 </a>
                 <p id="date">
@@ -21,12 +22,13 @@
                 </p>
                 <div class="arrow bounce">
                     <a href="#aboutus"><i class="fas fa-angle-double-down fa-2x"></i></a>
-                  </div>
                 </div>
-            <div class="col-lg-4 col-sm-12">
-                <!--Animation -->
+            </div>
+            <div class="col-lg-6 col-sm-12">
+                <video type="video/mp4" src="{{asset('videos/animation.mp4')}}" autoplay="" loop="" playsinline=""
+                    aria-hidden="true" style="width: 100%;vertical-align: middle;"></video>
             </div>
         </div>
-        
+
     </div>
 </div>
