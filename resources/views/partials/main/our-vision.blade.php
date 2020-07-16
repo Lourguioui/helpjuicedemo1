a0cb2c53c9ce961b6a689fe0269802e474654f1f
Alaeddine <ia_nasri@esi.dz>
2020-07-16 22:45:05 +0200

    Wall button text fixing about us and what zellat ruined lol

---
+++ resources/views/partials/main/our-vision.blade.php
@@ -0,0 +1,35 @@
+<section id="aboutus">
+    <div class="container">
+
+    <div class="row" class="ourvisiontext" >
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8" class="ourvisiontext">
+        <h1 class="title">Our Vision</h1>
+        <p id="ourpara"> 
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+        </div>
+    
+    <div class="col-lg-8"></div>
+</div>
+
+<a href="http://cse.club" target="_blank">
+                    <button>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Our Vision
+                    </button>
+                </a>
+
+
+                <div class="row" id="bigwall" >
+    
+    <div class="col-lg-6" >
+       <img src="{{asset('img/Shelf.png')}}" alt="idk" class="non-specified">
+    </div>
+</div>
+</div>
+</section>
+    
