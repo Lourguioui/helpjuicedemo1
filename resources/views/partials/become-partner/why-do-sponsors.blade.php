95e71a8b12cc3deff7291ea21c093a95d5aae61d
Raiah Mohamed Amine <hm_raiah@esi.dz>
2020-09-25 16:34:17 +0100

    DEV : Implementing Why Do Sponsors + CSS

---
+++ resources/views/partials/become-partner/why-do-sponsors.blade.php
@@ -0,0 +1,58 @@
+<section id="why">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <h1 class="title">Why do sponsors partner with CSE ?</h1>
+            </div>
+        </div>
+        <div class='row justify-content-center' style='position:relative'>
+                <p class='col-10 col-lg-7' style='margin-bottom:5%;font-size:100%'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Por
+                    ro id fuga non sint quam omnis excepturi eaque, amet explicabo! Corrupti,
+                    recusandae laborum? Delectus
+                    distinctio aspernatur cupiditate ad, qui autem dignissimos!
+                </p>
+                <div class='row justify-content-center' style='position:relative'>
+                    <div class='col-10 col-sm-8 col-lg-4 why-img-container'>
+                        <img class='why-img' src="{{asset('img/recruitment.png')}}"/>
+                        <h1 class='why-title' > Recruitment </h1>
+                        <p class='col-10 col-lg-10 why-p'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Por
+                                ro id fuga non sint quam omnis excturi : 
+                        </p>
+                        <ul class='why-p'> 
+                            <li> Lorem ipsum dolor sit </li>
+                            <li> Lorem ipsum dolor sit </li>
+                            <li> Lorem ipsum dolor sit </li>
+                        </ul>
+                    </div>
+                    <div class='col-10 col-sm-8 col-lg-4 why-img-container'>
+                        <img class='why-img' src="{{asset('img/marketing.png')}}"/>
+                        <h1 class='why-title' > Marketing </h1>
+                        <p class='col-10 col-lg-10 why-p'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Por
+                                ro id fuga non sint quam omnis excturi : 
+                        </p>
+                        <ul> 
+                            <li> Lorem ipsum dolor sit </li>
+                            <li> Lorem ipsum dolor sit </li>
+                            <li> Lorem ipsum dolor sit </li>
+                        </ul>
+                    </div>
+                    <div class='col-10 col-sm-8 col-lg-4 why-img-container'>
+                        <img class='why-img' src="{{asset('img/dev.png')}}"/>
+                        <h1 class='why-title' > Dev Relations </h1>
+                        <p class='col-10 col-lg-10 why-p'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Por
+                                ro id fuga non sint quam omnis excturi : 
+                        </p>
+                        <ul> 
+                            <li> Lorem ipsum dolor sit </li>
+                            <li> Lorem ipsum dolor sit </li>
+                            <li> Lorem ipsum dolor sit </li>
+                        </ul>
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
