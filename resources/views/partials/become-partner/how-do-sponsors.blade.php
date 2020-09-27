e69be7177af86dfff7b61549e4adae34ce3d7a0f
Sehdi Nassim <hm_sehdi@esi.dz>
2020-09-27 22:26:55 +0100

    Added how-do-sponsors... section and updated footer (become-partner)

---
+++ resources/views/partials/become-partner/how-do-sponsors.blade.php
@@ -0,0 +1,48 @@
+<section>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="title col-10 col-lg-auto">How do sponsors partner with CSE ?</h1>
+        </div>
+        <div class="row justify-content-center">
+            <p class="col-10 col-lg-7" style="text-align: justify; text-align-last: center;margin-bottom: 60px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae adipisci ullam esse, illo maxime ipsum quod cum, obcaecati magnam quam dignissimos distinctio est explicabo tempore eius error eos at in?</p>
+        </div>
+        <div class="row justify-content-around">
+            <div class="col-11 col-sm-6 col-lg-4 p-4">
+                <div class="img-container">
+                    <img class="how-do-sponsors-img" src="{{asset('img/how-do.png')}}" alt="how do">
+                    <div class="img-label bold" style="background-color: #00C4FF;">lorem ipsum</div>
+                </div>
+            </div>
+            <div class="col-11 col-sm-6 col-lg-4 p-4">
+                <div class="img-container">
+                    <img class="how-do-sponsors-img" src="{{asset('img/how-do.png')}}" alt="how do">
+                    <div class="img-label bold" style="background-color: #D68583;">lorem ipsum</div>
+                </div>
+            </div>
+            <div class="col-11 col-sm-6 col-lg-4 p-4">
+                <div class="img-container">
+                    <img class="how-do-sponsors-img" src="{{asset('img/how-do.png')}}" alt="how do">
+                    <div class="img-label bold" style="background-color: #FFBC00;">lorem ipsum</div>
+                </div>
+            </div>
+            <div class="col-11 col-sm-6 col-lg-4 p-4">
+                <div class="img-container">
+                    <img class="how-do-sponsors-img" src="{{asset('img/how-do.png')}}" alt="how do">
+                    <div class="img-label bold" style="background-color: #EE9B0F;">lorem ipsum</div>
+                </div>
+            </div>
+            <div class="col-11 col-sm-6 col-lg-4 p-4">
+                <div class="img-container">
+                    <img class="how-do-sponsors-img" src="{{asset('img/how-do.png')}}" alt="how do">
+                    <div class="img-label bold" style="background-color: #02A5DC;">lorem ipsum</div>
+                </div>
+            </div>
+            <div class="col-11 col-sm-6 col-lg-4 p-4">
+                <div class="img-container">
+                    <img class="how-do-sponsors-img" src="{{asset('img/how-do.png')}}" alt="how do">
+                    <div class="img-label bold" style="background-color: #0B0C12;">lorem ipsum</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
