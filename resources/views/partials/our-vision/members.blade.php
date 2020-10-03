687b5d83b51735ed7565e0c41b71cb0ad351f751
z-sohaib <js_zouambia@esi.dz>
2020-10-03 11:11:55 +0100

    Second Part of Our Vision Page

---
+++ resources/views/partials/our-vision/members.blade.php
@@ -0,0 +1,33 @@
+<section id="members">
+    <div class="container">
+        <div class="row pt-5 align-items-center justify-content-center">
+            <div class="col-10 col-md-6 mb-5 mb-md-0">
+                <div class="row justify-content-center justify-content-md-start">
+                    <div class="col-auto p-0">
+                        <h3 class="title">
+                            Members.
+                        </h3>
+                    </div>
+                    <p>
+                    Lorem ipsum dolor sit amet, 
+                    consetetur sadipscing elitr, sed diam nonumy 
+                    eirmod tempor invidunt ut labore et dolore magna 
+                    aliquyam erat, sed diam voluptua. At vero eos et
+                    </p>
+                    <a href="#" target="_blank">
+                        <button class="mt-2">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            See More
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img class='members-img' src="{{asset('img/Members.png')}}"/>
+            </div>
+        </div>
+    </div>
+</section>
