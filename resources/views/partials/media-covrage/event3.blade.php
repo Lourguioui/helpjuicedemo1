1dab88d8f0ae4209a5f7b03304f148b863b1104c
Aladdin <ia_nasri@esi.dz>
2020-10-17 13:25:10 +0200

    Full page without videos

---
+++ resources/views/partials/media-covrage/event3.blade.php
@@ -0,0 +1,45 @@
+<section id="event3">
+    <div class="container">
+        <div class="row pt-5 justify-content-center align-items-center">
+            <div class="col-12 col-md-6">
+                <img src="{{asset('img/media.png')}}" class="img-fluid" alt="Responsive image">
+            </div>
+            <div class="col-10 col-md-6">
+                <div class="row justify-content-center justify-content-md-start">
+                    <div class="col-auto p-0">
+                        
+                        <p> Passage radio - Joradio TV-</p>
+                        <h3 class="title">
+                        About event3
+                        </h3>
+                        <h6 class="media-date" > 28 Avril 2015 </h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum odio quae voluptates dignissimos
+                        voluptatibus fuga vel, necessitatibus quidem? Sit, dicta.
+                    </p>
+                </div>
+                <div class="avatars">
+                    <img src="{{asset('img/avatar.jpg')}}" alt="Avatar" class="avatar">
+                    <img src="{{asset('img/avatar.jpg')}}" alt="Avatar" class="avatar">
+                    <img src="{{asset('img/avatar.jpg')}}" alt="Avatar" class="avatar">
+                </div>
+                <a href="{{route('media')}}" target="_blank">
+                        <button class="mt-2" id="small-button">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            See the passage !
+                        </button>
+                </a>
+            </div>
+        </div>
+        <div>
+        <!-- <video width="320" height="240" autoplay loop>
+            <source src="movie.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video> -->
+        </div>
+    </div>
+</section>
---
+++ resources/views/partials/media-covrage/hackit2.blade.php
@@ -0,0 +1,47 @@
+<section id="about-hackit2">
+    <div class="container">
+        <div class="row pt-5 justify-content-center align-items-center">
+            <div class="col-12 col-md-6">
+                <img src="{{asset('img/media.png')}}" class="img-fluid" alt="Responsive image">
+            </div>
+            <div class="col-10 col-md-6">
+                <div class="row justify-content-center justify-content-md-start">
+                        
+                    
+                    <div class="col-auto p-0" >
+                        <p> Passage TV -Dzair News TV-</p>
+                        <h3 class="title">
+                            About Hack!T 2
+                        </h3>
+                        <h6 class="media-date" > 16 Avril 2018 </h6>
+                    </div>
+                    
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum odio quae voluptates dignissimos
+                        voluptatibus fuga vel, necessitatibus quidem? Sit, dicta.
+                    </p>
+                </div>
+                <div class="avatars">
+                    <img src="{{asset('img/avatar.jpg')}}" alt="Avatar" class="avatar">
+                    <img src="{{asset('img/avatar.jpg')}}" alt="Avatar" class="avatar">
+                    <img src="{{asset('img/avatar.jpg')}}" alt="Avatar" class="avatar">
+                </div>
+                <a href="{{route('media')}}" target="_blank">
+                        <button class="mt-2" id="small-button">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            See the passage !
+                        </button>
+                </a>
+            </div>
+        </div>
+        <div>
+        <!-- <video width="320" height="240" autoplay loop>
+            <source src="movie.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video> -->
+        </div>
+    </div>
+</section>
